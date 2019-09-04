1d7145110088609606238acc7c5696cc42acea7c
AMAN SHARMA <amansharmaammu2@gmail.com>
2019-09-04 15:01:29 +0530

    PG Set

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 # config.assets.initialize_on_precompile = false
+config.public_file_server.enabled = true
 
 module TimePass
   class Application < Rails::Application
